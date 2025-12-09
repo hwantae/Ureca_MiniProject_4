c54b9435ec35c5b921992d52c4d4d0b4b896d05f
hwantae <kwon99814@naver.com>
2025-12-09 12:14:41 +0900

    project setting

---
+++ src/main/java/com/mycom/myapp/UrecaMiniProject4Application.java
@@ -0,0 +1,13 @@
+package com.mycom.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrecaMiniProject4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrecaMiniProject4Application.class, args);
+	}
+
+}
