b3c188eaca560b9ad9fef8c045cb4a6a2272eeea
hwantae <kwon99814@naver.com>
2025-12-15 02:17:22 +0900

    로그인 후 auth 토큰 체크 에러 수정 및 보드 프론트 쪽 작성

--- src/main/java/com/mycom/myapp/board/BoardController.java
+++ src/main/java/com/mycom/myapp/board/BoardController.java
@@ -1,4 +1,4 @@
-package board.controller;
+package com.mycom.myapp.board;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
