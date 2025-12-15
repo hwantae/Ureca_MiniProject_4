5e5537705e1d2f94a67a0d10cb3e60cffa4d6cba
hwantae <kwon99814@naver.com>
2025-12-15 11:39:40 +0900

    admin-rooms 권한설정

--- src/main/java/com/mycom/myapp/auth/service/LoginServiceImpl.java
+++ src/main/java/com/mycom/myapp/auth/service/LoginServiceImpl.java
@@ -1,7 +1,10 @@
 package com.mycom.myapp.auth.service;
 
 import com.mycom.myapp.auth.dto.LoginResultDto;
+import com.mycom.myapp.domain.Users;
 import com.mycom.myapp.jwt.JwtUtil;
+import com.mycom.myapp.users.dto.UserDto;
+import com.mycom.myapp.users.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -17,6 +20,7 @@ import java.util.List;
 @Slf4j
 public class LoginServiceImpl implements LoginService{
     private final AuthenticationManager authenticationManager;
+    private final UserRepository userRepository;
 
     private final JwtUtil jwtUtil;
     @Override
@@ -31,10 +35,20 @@ public class LoginServiceImpl implements LoginService{
             List<String> roles = authentication.getAuthorities().stream()
                     .map(GrantedAuthority::getAuthority).toList();
 
+            Users users = userRepository.findByEmail(email).orElseThrow(()->new RuntimeException("User not found"));
+
+            UserDto userDto = UserDto.builder()
+                    .id(users.getUserId())        // User 엔티티의 ID getter (getUserId 혹은 getId 확인 필요)
+                    .email(users.getEmail())
+                    .name(users.getName())
+                    .role(roles)                 // 위에서 추출한 role 리스트 바로 사용
+                    .build();
+
             String token = jwtUtil.createToken(username, roles);
             System.out.println(token);
             loginResultDto.setResult("success");
             loginResultDto.setToken(token);
+            loginResultDto.setUserDto(userDto);
         } catch(Exception e){
             loginResultDto.setResult("fail");
         }
